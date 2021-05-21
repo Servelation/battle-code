e3a70571809da00e9043947c63b383c994bee487
Ne4ay <A-lex00-7@yandex.ua>
2021-05-21 14:03:24 +0300

    added username in header

--- battlecode/src/main/java/ru/nechay/practice/battlecode/controllers/MainController.java
+++ battlecode/src/main/java/ru/nechay/practice/battlecode/controllers/MainController.java
@@ -1,25 +1,30 @@
 package ru.nechay.practice.battlecode.controllers;
 
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.authentication.RememberMeAuthenticationToken;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import ru.nechay.practice.battlecode.models.User;
+
 @Controller
 public class MainController {
 
 	
-	@GetMapping("/main")
-	public String main(Model model) {
+	@GetMapping(value={"/main", "/"})
+	public String main(@AuthenticationPrincipal User user,
+					Model model) {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		System.out.println(authentication.getClass());
 		Boolean isAuthificated = UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication.getClass());
 		System.out.println(isAuthificated);
+		
+		
 		model.addAttribute("isAuth",isAuthificated);
+		model.addAttribute("user", user);
 		return "battlecode/main";
 	}
 }
